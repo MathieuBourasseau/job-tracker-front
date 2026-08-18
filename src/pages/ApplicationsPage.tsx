62ddd60cc9a3b1820f8363a28d2f6bbf29695f65
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-18 15:54:47 +0200

    feat: useEffect added in ApplicationPage.tsx in order to load all applications each time the page is loaded.

--- src/pages/ApplicationsPage.tsx
+++ src/pages/ApplicationsPage.tsx
@@ -1,4 +1,56 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth"
+
 export default function ApplicationsPage() {
+
+    // Extract token from context
+    const { token } = useAuth();
+
+    // States
+    const [applicationList, setApplicationList] = useState();
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    // API URL
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    // Load applications lists once when the page is shown
+    useEffect(() => {
+
+        // Function to get application from back end
+        async function fetchApplications(){
+
+            try {
+
+                // Send request to back end to get application list
+                const response = await fetch(`${API_URL}/api/applications`, {
+                    method: "GET",
+                    headers: { 
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}` 
+                    },
+                });
+
+                const data = await response.json();
+
+                // Condition to see what to do with response
+                if (response.ok) {
+                    // Upload application list
+                    setApplicationList(data)
+                } else {
+                    setErrorMessage(data);
+                }
+                
+            } catch (error) {
+                console.error(error)
+                setErrorMessage("Impossible de joindre le serveur")
+            }
+        }
+
+        fetchApplications();
+
+    },[])
+
+
     return (
         <div>
             <h1>Mes candidatures</h1>
